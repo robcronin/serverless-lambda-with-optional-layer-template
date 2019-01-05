9e49d05a3622b2403956e09a2d5bbff078190776
robcronin <robc@theodo.co.uk>
2019-01-05 16:51:17 +0000

    init

---
+++ handler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export const hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+    }),
+  };
+};
